6b64b174657b19241c927943446d7b38bf49cf6b
user234693 <user234693@NY545.local>
2023-10-10 11:03:43 -0400

    initial commit

---
+++ StoryWings/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryWings
+//
+//  Created by user234693 on 10/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
